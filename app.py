4a37ee6b3b049c76cb5d606f7dc7ac5ea575a32b
Atharva <atharva.raibagi1@gmail.com>
2024-03-05 00:15:54 +0530

    Webapp using streamlit

--- app.py
+++ app.py
@@ -5,3 +5,13 @@ import pickle
 import tensorflow
 from tensorflow import keras
 
+with open('ANN_Loan_Prediction.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with open('Loan_Prediction_encoder.pkl', 'rb') as file:
+    encoder = pickle.load(file)
+
+with open('Loan_Prediction_scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+
